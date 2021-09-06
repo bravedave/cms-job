24238ab779d251369b63836a00a47242ddbfc5c8
Dave Bray <david@brayworth.com.au>
2021-09-06 15:58:34 +1000

    add index

--- src/job/config.php
+++ src/job/config.php
@@ -14,7 +14,7 @@ namespace cms\job;
 use currentUser;
 
 class config extends \config {
-	const cms_job_db_version = 5.3;
+	const cms_job_db_version = 5.4;
 
 	const label = 'JOB';
 	const label_contractor_add = 'New Contractor';
